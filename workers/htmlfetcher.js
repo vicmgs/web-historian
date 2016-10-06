7e23d1a000fa6bd1a15c8b9dee81991714a9a125
vicmgs <vic.mgs@gmail.com>
2016-10-06 13:32:50 -0700

    Set up cron, download worker able to run

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,9 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+
+archive.readListOfUrls(function(urls) {
+  archive.downloadUrls(urls);
+});
